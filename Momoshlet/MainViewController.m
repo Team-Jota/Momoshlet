b8fbcef59a9e715a5e686949d7985e4b80998273
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-06 20:10:04 +0900

    いろいろミスりました

--- Momoshlet/MainViewController.m
+++ Momoshlet/MainViewController.m
@@ -14,7 +14,6 @@
 
 @implementation MainViewController
 
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,13 +26,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    btn = [UIButton buttonWithType:0];
-    btn.frame = CGRectMake(0, 0, 50, 30);
-    V
-    
-    
-    
 	// Do any additional setup after loading the view.
     
     self.view.backgroundColor = [UIColor blackColor];
