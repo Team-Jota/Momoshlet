620010c51f19cf21f23a530a2282b338c41511f9
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-07 17:51:33 +0900

    追加しました

---
+++ Momoshlet/CatchBugView.m
@@ -0,0 +1,31 @@
+//
+//  CatchBugView.m
+//  Momoshlet
+//
+//  Created by 鈴木 大貴 on 2012/09/07.
+//  Copyright (c) 2012年 鈴木 大貴. All rights reserved.
+//
+
+#import "CatchBugView.h"
+
+@implementation CatchBugView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
