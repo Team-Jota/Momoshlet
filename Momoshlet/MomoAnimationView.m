3725fed26deadca3d93fc4ea1491fc79919751c6
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-09 00:30:46 +0900

    buttonに動きをつけました

--- Momoshlet/MomoAnimationView.m
+++ Momoshlet/MomoAnimationView.m
@@ -10,13 +10,76 @@
 
 @implementation MomoAnimationView
 
-- (id)initWithFrame:(CGRect)frame
+- (id)initWithMomoButton:(UIButton *)btn
 {
-    self = [super initWithFrame:frame];
+    self = [super initWithFrame:btn.frame];
     if (self) {
-        // Initialization code
+        isAnimation = YES;
+        [self addSubview:btn];
     }
     return self;
+}
+
+- (void)animation4{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDelay:0.01];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationsEnabled:isAnimation];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(statAniamtion)];
+    
+    self.center = CGPointMake(self.center.x+10, self.center.y);
+    self.transform = CGAffineTransformMakeRotation(0.0);
+    
+    [UIView commitAnimations];
+}
+
+- (void)animation3{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationsEnabled:isAnimation];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(animation4)];
+    
+    self.center = CGPointMake(self.center.x-10, self.center.y);
+    self.transform = CGAffineTransformMakeRotation(0.25);
+    
+    [UIView commitAnimations];
+}
+
+- (void)animation2{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDelay:0.01];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationsEnabled:isAnimation];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(animation3)];
+    
+    self.center = CGPointMake(self.center.x-10, self.center.y);
+    self.transform = CGAffineTransformMakeRotation(0.0);
+    
+    [UIView commitAnimations];
+}
+
+- (void)statAniamtion{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationsEnabled:isAnimation];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(animation2)];
+    
+    self.center = CGPointMake(self.center.x+10, self.center.y);
+    self.transform = CGAffineTransformMakeRotation(-0.25);
+    
+    [UIView commitAnimations];
+}
+
+- (void)stopAniamtion{
+    isAnimation = NO;
 }
 
 /*
