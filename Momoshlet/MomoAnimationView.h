9868836ddb207fd0589a15721bfca60c912fc9df
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-08 17:07:52 +0900

    add MomoAnimationView.h

---
+++ Momoshlet/MomoAnimationView.h
@@ -0,0 +1,13 @@
+//
+//  MomoAnimationView.h
+//  Momoshlet
+//
+//  Created by 鈴木 大貴 on 2012/09/08.
+//  Copyright (c) 2012年 鈴木 大貴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MomoAnimationView : UIView
+
+@end
