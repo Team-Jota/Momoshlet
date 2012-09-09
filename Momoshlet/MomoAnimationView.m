42a6d4d8052492bb948a97ddbafaa86f4a3e4b4b
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-09 11:22:06 +0900

    amimation用のコードを追加

--- Momoshlet/MomoAnimationView.m
+++ Momoshlet/MomoAnimationView.m
@@ -7,6 +7,7 @@
 //
 
 #import "MomoAnimationView.h"
+#define INTERVAL 1
 
 @implementation MomoAnimationView
 
@@ -15,7 +16,15 @@
     self = [super initWithFrame:btn.frame];
     if (self) {
         isAnimation = YES;
-        [self addSubview:btn];
+        saveData = [SaveData initSaveData];
+        buttonSize = btn.frame.size;
+        index = (btn.tag/10)-1;
+        
+        scaleView = [[UIView alloc] initWithFrame:btn.frame];
+        scaleView.transform = CGAffineTransformMakeScale(0, 0);
+        [scaleView addSubview:btn];
+        
+        [self addSubview:scaleView];
     }
     return self;
 }
@@ -30,7 +39,7 @@
     [UIView setAnimationDidStopSelector:@selector(statAniamtion)];
     
     self.center = CGPointMake(self.center.x+10, self.center.y);
-    self.transform = CGAffineTransformMakeRotation(0.0);
+    self.transform = CGAffineTransformRotate(self.transform, -0.25);
     
     [UIView commitAnimations];
 }
@@ -44,7 +53,7 @@
     [UIView setAnimationDidStopSelector:@selector(animation4)];
     
     self.center = CGPointMake(self.center.x-10, self.center.y);
-    self.transform = CGAffineTransformMakeRotation(0.25);
+    self.transform = CGAffineTransformRotate(self.transform, 0.25);
     
     [UIView commitAnimations];
 }
@@ -59,7 +68,7 @@
     [UIView setAnimationDidStopSelector:@selector(animation3)];
     
     self.center = CGPointMake(self.center.x-10, self.center.y);
-    self.transform = CGAffineTransformMakeRotation(0.0);
+    self.transform = CGAffineTransformRotate(self.transform, 0.25);
     
     [UIView commitAnimations];
 }
@@ -73,13 +82,28 @@
     [UIView setAnimationDidStopSelector:@selector(animation2)];
     
     self.center = CGPointMake(self.center.x+10, self.center.y);
-    self.transform = CGAffineTransformMakeRotation(-0.25);
+    self.transform = CGAffineTransformRotate(self.transform, -0.25);
     
     [UIView commitAnimations];
 }
 
 - (void)stopAniamtion{
     isAnimation = NO;
+}
+
+- (void)growMomo{
+    NSDictionary *status = [saveData.statusArray objectAtIndex:index];
+    float since = [[NSDate date] timeIntervalSinceDate:[status objectForKey:@"created_at"]];
+    float finishTime = [[status objectForKey:@"hours"] floatValue]*60*60;
+    float size = since / finishTime;
+    
+    if (size < 1.0){
+        scaleView.transform = CGAffineTransformMakeScale(size, size);
+        [self performSelector:@selector(growMomo) withObject:nil afterDelay:INTERVAL];
+    }
+    else {
+        scaleView.transform = CGAffineTransformMakeScale(1.0, 1.0);
+    }
 }
 
 /*
