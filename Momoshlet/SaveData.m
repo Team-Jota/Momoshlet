817f1cf6d4d92abe8eee37a8725d9c9c397a06eb
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-09 17:02:26 +0900

    いろいろ削除

--- Momoshlet/SaveData.m
+++ Momoshlet/SaveData.m
@@ -10,18 +10,15 @@
 
 #define COLLECTION 0
 #define STATUS 1
+#define MONEY 2
 
 @implementation SaveData
 
 @synthesize collectionArray;
 @synthesize statusArray;
+@synthesize money;
 
 SaveData *saveData = nil;
-
-const int nomal = 0;
-const int dry = 1;
-const int wet = 2;
-const int injury = 3;
 
 + (id)initSaveData
 {
@@ -42,6 +39,9 @@ const int injury = 3;
     else if (status==STATUS){
         [nud setObject:statusArray forKey:@"status"];
     }
+    else if (status==MONEY){
+        [nud setObject:[NSNumber numberWithInt:money] forKey:@"money"];
+    }
     
     [nud synchronize];
 }
@@ -49,6 +49,15 @@ const int injury = 3;
 - (void)loadSaveData
 {
     NSUserDefaults *nud = [NSUserDefaults standardUserDefaults];
+    
+    if ([nud objectForKey:@"money"]!=NULL) {
+        money = [[nud objectForKey:@"money"] intValue];
+    }
+    else {
+        money = 1000;
+        
+        [self saveSaveData:MONEY];
+    }
     
     if ([nud objectForKey:@"collection"]!=NULL) {
         collectionArray = [[NSMutableArray alloc] initWithArray:[nud objectForKey:@"collection"]];
@@ -73,17 +82,40 @@ const int injury = 3;
             NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
             [dic setValue:[NSNumber numberWithInt:100] forKey:@"id"];
             [dic setValue:[NSNumber numberWithFloat:0.5] forKey:@"hours"];
-            [dic setValue:[NSNumber numberWithInt:nomal] forKey:@"status"];
             [dic setValue:[NSDate date] forKey:@"created_at"];
-            [dic setValue:[NSNumber numberWithInt:0] forKey:@"injury"];
-            [dic setValue:[NSNumber numberWithInt:5] forKey:@"moisture"];
-            [dic setValue:[NSNumber numberWithInt:0] forKey:@"dirty"];
+            [dic setValue:[NSNumber numberWithInt:0] forKey:@"injury_level"];//5段階
+            [dic setValue:[NSNumber numberWithInt:5] forKey:@"moisture_level"];//10段階
+            [dic setValue:[NSNumber numberWithInt:0] forKey:@"dirty_level"];//5段階
+            [dic setValue:[NSNumber numberWithInt:0] forKey:@"injury_resistance"];//5段階
+            [dic setValue:[NSNumber numberWithInt:5] forKey:@"moisture_resistance"];//10段階
+            [dic setValue:[NSNumber numberWithInt:0] forKey:@"dirty_resistance"];//5段階
             
             [statusArray addObject:dic];
         }
         
         [self saveSaveData:STATUS];
     }
+}
+
+- (void)makeNewMomo:(int)index
+{
+    [statusArray insertObject:nil atIndex:index];
+    
+    NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
+    [dic setValue:[NSNumber numberWithInt:100] forKey:@"id"];
+    [dic setValue:[NSNumber numberWithFloat:0.5] forKey:@"hours"];
+    //[dic setValue:[NSNumber numberWithInt:[self getNormal]] forKey:@"status"];
+    [dic setValue:[NSDate date] forKey:@"created_at"];
+    [dic setValue:[NSNumber numberWithInt:0] forKey:@"injury_level"];//5段階
+    [dic setValue:[NSNumber numberWithInt:5] forKey:@"moisture_level"];//10段階
+    [dic setValue:[NSNumber numberWithInt:0] forKey:@"dirty_level"];//5段階
+    [dic setValue:[NSNumber numberWithInt:0] forKey:@"injury_resistance"];//5段階
+    [dic setValue:[NSNumber numberWithInt:5] forKey:@"moisture_resistance"];//10段階
+    [dic setValue:[NSNumber numberWithInt:0] forKey:@"dirty_resistance"];//5段階
+    
+    [statusArray insertObject:dic atIndex:index];
+    
+    [self saveSaveData:STATUS];
 }
 
 @end
