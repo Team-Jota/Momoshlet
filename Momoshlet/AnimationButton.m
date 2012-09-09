3b5073a83ecf0067d86aaf2e26e2728045cbe502
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-09 11:23:49 +0900

    .gitignoreに内容追加

---
+++ Momoshlet/AnimationButton.m
@@ -0,0 +1,31 @@
+//
+//  AnimationButton.m
+//  Momoshlet
+//
+//  Created by 鈴木 大貴 on 2012/09/08.
+//  Copyright (c) 2012年 鈴木 大貴. All rights reserved.
+//
+
+#import "AnimationButton.h"
+
+@implementation AnimationButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
