4622711980c126320b792a3838f2126a98cb5730
北沢 匠 <takuchan@chantakusanjou-kitazawa-no-MacBook-Air.local>
2012-09-10 19:44:00 +0900

    加速度を取得するコードなどを追加しました。

--- Momoshlet/WashletView.m
+++ Momoshlet/WashletView.m
@@ -7,8 +7,11 @@
 //
 
 #import "WashletView.h"
+#define maxSpeed 0.1
 
 @implementation WashletView
+
+@synthesize img;
 
 - (id)initWithDelegate:(id)_delegate
 {
@@ -18,15 +21,62 @@
         delegate = _delegate;
         self.backgroundColor = [UIColor blackColor];
         
+        currentSpeed = 0;
+        
+        img = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"momo1-2.png"]];
+        img.frame = CGRectMake(50,50,100,100);
+        [self addSubview:img];
+        ballRadius = img.frame.size.width/2;
+        delta = CGPointMake(12.0, 4.0);
+        translation = CGPointMake(0.0, 0.0);
+        
         UIButton *rmButton = [cb makeButton:CGRectMake(0, 0, 50, 50) :@selector(callRemoveWashletView) :100 :nil];
         rmButton.backgroundColor = [UIColor blueColor];
         [self addSubview:rmButton];
+        
+        UIAccelerometer *accel = [UIAccelerometer sharedAccelerometer];
+        accel.delegate = self;
+        accel.updateInterval = 1.0f/60.0f;
     }
     return self;
 }
 
+
+-(void) accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration{
+    //NSLog(@"x: %g",acceleration.x);
+    //NSLog(@"y: %g",acceleration.y);
+    //NSLog(@"z: %g",acceleration.z);
+    
+    if(acceleration.x>0) delta.x=5;else delta.x=-5;
+    if(acceleration.y>0) delta.y=-5;else delta.y=5;
+    
+    [UIView beginAnimations:@"translate" context:nil];
+    img.transform =
+    CGAffineTransformMakeTranslation(translation.x, translation.y);
+    translation.x = translation.x + delta.x;
+    translation.y = translation.y + delta.y;
+    [UIView commitAnimations];
+    
+    if (img.center.x + translation.x > 320 - ballRadius ||
+        img.center.x + translation.x < ballRadius) {
+        translation.x -= delta.x;
+    }
+    if (img.center.y + translation.y > 460 - ballRadius ||
+        img.center.y + translation.y < ballRadius) {
+        translation.y -= delta.y;
+    }
+}
+
+-(float) calcNewSpeed{
+    
+    
+    
+    return currentSpeed;
+}
+
 - (void)callRemoveWashletView
 {
+    [[UIAccelerometer sharedAccelerometer]setDelegate:nil];
     [delegate removeWashletView];
 }
 
