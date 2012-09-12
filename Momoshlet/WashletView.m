fd3584bbab3e24a47c325e1f329e1c6147a445b4
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-12 10:43:39 +0900

    synthesizeを削除

--- Momoshlet/WashletView.m
+++ Momoshlet/WashletView.m
@@ -13,8 +13,6 @@
 
 @implementation WashletView
 
-@synthesize nozzleImg;
-
 - (id)initWithDelegate:(id)_delegate:(int)_index
 {
     self = [super initWithFrame:CGRectMake(0, 0, 320, 480)];
@@ -24,15 +22,12 @@
         delegate = _delegate;
         self.backgroundColor = [UIColor whiteColor];
         
-        NSDictionary *status = [saveData.statusArray objectAtIndex:index];
-        dirtyLevel = [[status objectForKey:@"dirty_level"]intValue];
         index = _index;
+        NSDictionary *status = [saveData.statusArray objectAtIndex:index];
+        dirtyLevel = [[status objectForKey:@"dirty_level"] intValue];
         
         isFinish = NO;
-        
-        UIButton *rmButton = [cb makeButton:CGRectMake(0, 0, 50, 50) :@selector(callRemoveWashletView) :100 :nil];
-        rmButton.backgroundColor = [UIColor blueColor];
-        [self addSubview:rmButton];
+        isAnimation = YES;
         
         //桃
         momoView = [[UIView alloc] initWithFrame:CGRectMake(BETWEEN, 30, 250, 250)];
@@ -58,7 +53,6 @@
         waterView.frame = CGRectMake(0, -150, 100, 300);
         [moveView addSubview:waterView];
         
-        isAnimation = YES;
         [self waterAnimation];
         
         //汚れの反映
@@ -68,12 +62,7 @@
         nozzleImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"momo1-1.png"]];
         nozzleImg.frame = CGRectMake(0, 0, 100, 100);
         [moveView addSubview:nozzleImg];
-        //nozzleImg.frame = CGRectMake(110,350,100,100);
-        //[self addSubview:nozzleImg];
-        //imgRadius = nozzleImg.frame.size.width/2;
-        //delta = CGPointMake(0.0, 0.0);
-        //translation = nozzleImg.center;
-        
+
         UIAccelerometer *accel = [UIAccelerometer sharedAccelerometer];
         accel.delegate = self;
         accel.updateInterval = 1.0f/60.0f;
@@ -117,61 +106,7 @@
         dirty5.frame = rect;
         [momoView addSubview:dirty5];
     }
-    
-    //dirty3.alpha = 0.2;
-    NSLog(@"dirty1 coordinate:%f",dirty5.center.x);
 }
-    
-
-/*- (void)washMomo
-{
-    NSDictionary *status = [saveData.statusArray objectAtIndex:index];
-    int indexCount = [[status objectForKey:@"dirty_level"]integerValue];
-    
-    if(indexCount>=1){
-        if(dirty1.alpha==0){
-            [self callRemoveWashletView];
-        }
-    }
-    if(indexCount>=2){
-        if(dirty1.alpha==0){
-            [self callRemoveWashletView];
-        }
-    }
-    if(indexCount>=3){
-        if(dirty1.alpha==0){
-            [self callRemoveWashletView];
-        }
-    }
-    if(indexCount>=4){
-        if(dirty1.alpha==0){
-            [self callRemoveWashletView];
-        }
-    }
-    if(indexCount>=5){
-        if(dirty1.alpha==0){
-            [self callRemoveWashletView];
-        }
-    }
-    
-    
-    if((35<=translation.x)&&(translation.x<85)){
-        
-    }
-    if((85<=translation.x)&&(translation.x<135)){
-        
-    }
-    if((135<=translation.x)&&(translation.x<185)){
-        
-    }
-    if((185<=translation.x)&&(translation.x<235)){
-        
-    }
-    if((235<=translation.x)&&(translation.x<285)){
-        
-    }
-}*/
-
 
 - (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration{
     
@@ -197,27 +132,27 @@
     moveView.center = CGPointMake(translation.x , translation.y);
     
     if (moveView.center.x > 73+BETWEEN && moveView.center.x <= 98+BETWEEN && dirty5!=nil && dirty5.alpha > 0) {
-        dirty5.alpha -= 0.005;
+        dirty5.alpha -= 0.02;
         momoHitImg.hidden = NO;
         momoNotHitImg.hidden = YES;
     }
     else if (moveView.center.x > 98+BETWEEN && moveView.center.x <= 117+BETWEEN && dirty3!=nil && dirty3.alpha > 0) {
-        dirty3.alpha -= 0.005;
+        dirty3.alpha -= 0.02;
         momoHitImg.hidden = NO;
         momoNotHitImg.hidden = YES;
     }
     else if (moveView.center.x > 117+BETWEEN && moveView.center.x <= 137+BETWEEN && dirty1!=nil && dirty1.alpha > 0) {
-        dirty1.alpha -= 0.005;
+        dirty1.alpha -= 0.02;
         momoHitImg.hidden = NO;
         momoNotHitImg.hidden = YES;
     }
     else if (moveView.center.x > 137+BETWEEN && moveView.center.x <= 160+BETWEEN && dirty2!=nil && dirty2.alpha > 0) {
-        dirty2.alpha -= 0.005;
+        dirty2.alpha -= 0.02;
         momoHitImg.hidden = NO;
         momoNotHitImg.hidden = YES;
     }
     else if (moveView.center.x > 160+BETWEEN && moveView.center.x <= 184+BETWEEN && dirty4!=nil && dirty4.alpha > 0) {
-        dirty4.alpha -= 0.005;
+        dirty4.alpha -= 0.02;
         momoHitImg.hidden = NO;
         momoNotHitImg.hidden = YES;
     }
@@ -226,8 +161,7 @@
         momoNotHitImg.hidden = NO;
     }
     
-    NSLog(@"1 = %f, 2 = %f, 3 = %f, 4 = %f, 5 = %f"
-          ,dirty1.alpha,dirty2.alpha,dirty3.alpha,dirty4.alpha,dirty5.alpha);
+    //NSLog(@"1 = %f, 2 = %f, 3 = %f, 4 = %f, 5 = %f",dirty1.alpha,dirty2.alpha,dirty3.alpha,dirty4.alpha,dirty5.alpha);
     
     if (isFinish==NO) {
         if (dirtyLevel==1) {
@@ -269,7 +203,6 @@
     [UIView setAnimationCurve:UIViewAnimationCurveLinear];
     [UIView setAnimationDelay:0.01];
     [UIView setAnimationDuration:0.5];
-    //[UIView setAnimationsEnabled:isAnimation];
     [UIView setAnimationDelegate:self];
     if (isAnimation) {
         [UIView setAnimationDidStopSelector:@selector(waterAnimation)];
@@ -286,7 +219,6 @@
     [UIView setAnimationCurve:UIViewAnimationCurveLinear];
     [UIView setAnimationDelay:0.01];
     [UIView setAnimationDuration:0.5];
-    //[UIView setAnimationsEnabled:isAnimation];
     [UIView setAnimationDelegate:self];
     if (isAnimation) {
         [UIView setAnimationDidStopSelector:@selector(waterAnimation2)];
