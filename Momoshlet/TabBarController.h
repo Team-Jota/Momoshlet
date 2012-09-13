c83ea64f681d29fbb90c423e5f17ae4bb35e338e
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-13 23:26:32 +0900

    追加

--- Momoshlet/TabBarController.h
+++ Momoshlet/TabBarController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TabBarController : UITabBarController
+@interface TabBarController : UITabBarController<UITabBarControllerDelegate> {
+    BOOL isCollectionViewController;
+}
 
 @end
