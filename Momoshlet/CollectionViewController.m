ed1095f2ba6cf95bab1efb14f52695b606b0bfb3
鈴木 大貴 <SuzukiTaiki@taikiMacBookPro-2.local>
2012-09-13 23:26:01 +0900

    追加

--- Momoshlet/CollectionViewController.m
+++ Momoshlet/CollectionViewController.m
@@ -27,11 +27,7 @@
 
 - (void)removeDetailView
 {
-    NSLog(@"removeDetailVIew");
-    
     if (detail) {
-        NSLog(@"in");
-        
         [detail removeFromSuperview];
         detail = nil;
     }
@@ -45,20 +41,25 @@
 {
     cb = [CustomButton initWithDelegate:self];
     
-    if (detail) {
-        [detail removeFromSuperview];
-        detail = nil;
-    }
-    
     detail = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
     detail.backgroundColor = [UIColor purpleColor];
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 50)];
-    [label setText:[NSString stringWithFormat:@"%d",btn.tag]];
-    [detail addSubview:label];
+    int number = (btn.tag/10)-1;
+    int momo_id = number/5;
+    int state = number%5;
     
-    UIButton *btn1 = [cb makeButton:CGRectMake(0, 100, 320, 50) :@selector(removeDetailView) :1000 :nil];
-    btn1.backgroundColor = [UIColor blackColor];
+    /*UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 50)];
+    [label setText:[NSString stringWithFormat:@"%d",btn.tag]];
+    [detail addSubview:label];*/
+    
+    NSLog(@"momo_id = %d, state = %d", momo_id, state);
+    
+    UIImageView *img = [[UIImageView alloc] initWithImage:[self makeImage:momo_id :state]];
+    img.frame = CGRectMake(0, 60.5, 320, 320);
+    [detail addSubview:img];
+    
+    UIButton *btn1 = [cb makeButton:CGRectMake(0, 0, 320, 480) :@selector(removeDetailView) :1000 :nil];
+    btn1.backgroundColor = [UIColor clearColor];
     [detail addSubview:btn1];
     
     [self.view addSubview:detail];
@@ -67,42 +68,97 @@
     [app tabBatItemMainEnabled:NO];
 }
 
-- (void)viewDidLoad
+- (UIImage *)makeImage:(int)index:(int)state
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    int number = 1;
+    
+    if (state==1|| state==2 || state==3){
+        number = 2;
+    }
+    else if (state == 4){
+        number = 3;
+    }
+    
+    UIGraphicsBeginImageContext(CGSizeMake(400, 400));
+    [[UIImage imageNamed:[NSString stringWithFormat:@"momo%d-%d.png", index+1, number]] drawInRect:CGRectMake(0, 0, 400, 400)];
+    if (state == 1 || state == 3) {
+        for (int i=1; i<=5; i++) {
+            [[UIImage imageNamed:[NSString stringWithFormat:@"dirty%d.png",i]] drawInRect:CGRectMake(0, 0, 400, 400)];
+        }
+    }
+    if (state == 2 || state == 3) {
+        for (int i=1; i<=5; i++) {
+            [[UIImage imageNamed:[NSString stringWithFormat:@"bug%d.png",i]] drawInRect:CGRectMake(0, 0, 400, 400)];
+        }
+    }
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return img;
+}
+
+- (void)makeScrollView
+{
+    pages = 5;
+    for (int k=25; k<30; k++) {
+        BOOL tempBool = [[saveData.collectionArray objectAtIndex:k] boolValue];
+        if (tempBool==YES) {
+            pages = 6;
+            break;
+        }
+    }
+    
+    if (stage) {
+        [stage removeFromSuperview];
+        stage = nil;
+    }
+    
     cb = [CustomButton initWithDelegate:self];
+    stage = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+    [self.view addSubview:stage];
     
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
     scrollView.delegate = self;
     
-    UIView *scrollStage = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320*5, 480)];
+    UIView *scrollStage = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320*pages, 480)];
     scrollView.contentSize = scrollStage.frame.size;
     scrollView.pagingEnabled = YES;
     
+    CGPoint point[5] = {
+        CGPointMake(23.3+62.5, 60.3+62.5),
+        CGPointMake(23.3+23.3+125+62.5, 60.3+62.5),
+        CGPointMake(23.3+63.5, 60.3+60.3+125+62.5),
+        CGPointMake(23.3+23.3+125+62.5, 60.3+60.3+125+62.5),
+        CGPointMake(160, 215.5)
+    };
+    
     int count = 1;
-    for (int i=0; i<5; i++) {
-        UIView *tempView = [[UIView alloc] initWithFrame:CGRectMake(320*i, 0, 320, 480)];
-        if (i%2 == 0){
-            tempView.backgroundColor = [UIColor blueColor];
-        }
-        else {
-            tempView.backgroundColor = [UIColor redColor];
-        }
+    for (int i=0; i<pages; i++) {
+        UIImageView *bgImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bgcollection1.png"]];
+        bgImg.frame = CGRectMake(320*i, 0, 320, 480);
+        [scrollStage addSubview:bgImg];
         
-        for (int j=0; j<2; j++) {
-            for (int k=0; k<2; k++) {
-                NSString *img;
-                img = @"momo_shade.png";
-                
-                UIButton *btn = [cb makeButton:CGRectMake(0, 0, 250, 250) :@selector(detailView:) :count*10 :img];
-                btn.transform = CGAffineTransformMakeScale(0.5, 0.5);
-                btn.center = CGPointMake(80+160*k, 120+200*j);
-                
-                [tempView addSubview:btn];
-                
-                count++;
+        UIView *tempView = [[UIView alloc] initWithFrame:CGRectMake(320*i, 0, 320, 480)];
+        tempView.backgroundColor = [UIColor clearColor];
+        
+        for (int j=0; j<5; j++) {
+            BOOL isCollection = [[saveData.collectionArray objectAtIndex:count-1] boolValue];
+            
+            UIButton *btn = [cb makeButton:CGRectMake(0, 0, 125, 125) :@selector(detailView:) :count*10 :nil];
+            UIImage *img;
+            if (isCollection==YES) {
+                img = [self makeImage:i :j];
             }
+            else {
+                img = [UIImage imageNamed:@"momo_shade.png"];
+                btn.enabled = NO;
+            }
+            [btn setImage:img forState:UIControlStateNormal];
+            btn.center = point[j];
+            
+            [tempView addSubview:btn];
+            
+            count++;
         }
         
         [scrollStage addSubview:tempView];
@@ -110,15 +166,31 @@
     
     [scrollView addSubview:scrollStage];
     
-    pageLagel = [[UILabel alloc] initWithFrame:CGRectMake(0, 270, 320, 50)];
-    pageLagel.text = @"1/5";
+    pageLagel = [[UILabel alloc] initWithFrame:CGRectMake(115, 381, 100, 50)];
+    pageLagel.text = [NSString stringWithFormat:@"1/%d",pages];
+    pageLagel.font = [UIFont fontWithName:@"MarkerFelt-Thin" size:50];
+    pageLagel.backgroundColor = [UIColor clearColor];
     page = 1;
     scrollXPoint = scrollView.contentOffset.x;
     detail = nil;
     
-    [self.view addSubview:scrollView];
+    [stage addSubview:scrollView];
     
-    [self.view addSubview:pageLagel];
+    [stage addSubview:pageLagel];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    cb = [CustomButton initWithDelegate:self];
+    saveData = [SaveData initSaveData];
+    stage = nil;
+    
+    [self makeScrollView];
 }
 
 - (void)viewDidUnload
@@ -149,15 +221,15 @@
         }
     }
     
-    if (page > 5) {
-        page = 5;
+    if (page > pages) {
+        page = pages;
     }
     
     if (page < 1) {
         page = 1;
     }
     
-    [pageLagel setText:[NSString stringWithFormat:@"%d/5",page]];
+    [pageLagel setText:[NSString stringWithFormat:@"%d/%d",page, pages]];
 }
 
 @end
